0049c53b36ee9c13f5f19ce101d3faf71e211c08
Leonardo <leonardoluz10@hotmail.com>
2022-10-14 22:28:08 -0300

    Verificando beneficios

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,26 +4,41 @@ describe('Barbearia Alura', () => {
     // Dado que ao acessar o link
     cy.visit('./../../src/index.html')
   })
-  it('Visitando links ancora da pagina inicial', () => {    
+  it('Visitando links ancora da pagina inicial', () => {
     cy.contains('a', 'Home')  // visita pagina inicial     
       .should('be.visible')
       .click()
-      .title()      
+      .title()
       .should('be.equal', 'Barbearia Alura')
     cy.contains('a', 'Produtos') // visita pagina de produtos
       .should('be.visible')
       .click()
       .title()
       .should('be.equal', 'Produtos - Barbearia Alura')
-      cy.contains('a', 'Contato') // visita pagina de contato
+    cy.contains('a', 'Contato') // visita pagina de contato
       .should('be.visible')
       .click()
       .title()
       .should('be.equal', 'Contato - Barbearia Alura')
   })
-  it.only('Verificando area do local do mapa',()=>{
-    cy.contains('p','Nosso estabelecimento está localizado no coração da cidade')
-    .should('be.visible')
+  it('Verificando area do local do mapa', () => {
+    cy.contains('p', 'Nosso estabelecimento está localizado no coração da cidade')
+      .should('be.visible')
+  })
+  it.only('Verificando beneficios', () => {
+    cy.contains('.lista-beneficios', 'Atendimentos aos clientes')
+      .should('be.visible')
+    cy.contains('.lista-beneficios', 'Espaço diferenciado')
+      .should('be.visible')
+    cy.contains('.lista-beneficios', 'Localização')
+      .should('be.visible')
+    cy.contains('.lista-beneficios', 'Profissionais Qualificados')
+      .should('be.visible')
+    cy.contains('.lista-beneficios', 'Pontualidade')
+      .should('be.visible')
+    cy.contains('.lista-beneficios', 'Limpeza')
+      .should('be.visible')
+
   })
 
 })
