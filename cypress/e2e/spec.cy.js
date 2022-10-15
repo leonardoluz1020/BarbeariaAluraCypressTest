8a3a47fa47425ddb4405ca696ddf87d4e685bf05
Leonardo <leonardoluz10@hotmail.com>
2022-10-14 22:13:37 -0300

    Visitando links ancora da pagina inicial

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,21 +5,25 @@ describe('Barbearia Alura', () => {
     cy.visit('./../../src/index.html')
   })
   it('Visitando links ancora da pagina inicial', () => {    
-    cy.contains('a', 'Home')       
+    cy.contains('a', 'Home')  // visita pagina inicial     
       .should('be.visible')
       .click()
       .title()      
       .should('be.equal', 'Barbearia Alura')
-    cy.contains('a', 'Produtos')
+    cy.contains('a', 'Produtos') // visita pagina de produtos
       .should('be.visible')
       .click()
       .title()
       .should('be.equal', 'Produtos - Barbearia Alura')
-      cy.contains('a', 'Contato')
+      cy.contains('a', 'Contato') // visita pagina de contato
       .should('be.visible')
       .click()
       .title()
       .should('be.equal', 'Contato - Barbearia Alura')
+  })
+  it.only('Verificando area do local do mapa',()=>{
+    cy.contains('p','Nosso estabelecimento está localizado no coração da cidade')
+    .should('be.visible')
   })
 
 })
