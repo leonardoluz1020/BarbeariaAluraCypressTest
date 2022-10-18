ca912ce6b6956725639fd8ff832ee68d5544c5db
Leonardo <leonardoluz10@hotmail.com>
2022-10-18 18:49:59 -0300

    Preenchendo formulario de contato

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -55,16 +55,19 @@ describe('Barbearia Alura', () => {
   it('Verificando video da pagina inicial', () => {
     cy.get('.video>iframe').should('be.visible')
   })
-  it('Fazendo uma requisição ao http', () => {
-    cy.request('https://www.youtube.com/watch?v=ILfdTXgV_Yk')
+  it('Fazendo uma requisição ao HTTP google maps', () => {
+    cy.request('https://www.google.com/maps/api/js/ApplicationService.GetEntityDetails?pb=!1m6!1m5!2sDigitalidade!3m2!1d-23.588766982687904!2d-46.63571834564209!4s9228955726054951659!2m2!1spt_BR!2sbr!4sNhNPY6agMKjb1sQP1NqckA4!7m1!2i3!13m1!4b1')
       // com request podemos fazer requisição a nivel de rede
       .should(function (response) { // usando o should para fazer verificações junto com uma função de callback que recebe a resposta da requisição, 
-        const { status, } = response // com a resposta da requisição estamos desestruturando o status
+        console.log(response);
+        const { status, statusText, isOkStatusCode, body } = response // com a resposta da requisição estamos desestruturando o status
         expect(status).to.equal(200) // com o expect verifica no status se é igual a 200
-
+        expect(statusText).to.equal('OK')
+        expect(isOkStatusCode).to.equal(true)
+        expect(body).to.include('Digitalidade - Rua Capitão Cavalcanti, 38 - Vila Mariana, São Paulo - SP, 04017-000')
       })
   })
-  it('Fazendo requisição usando method Post', () => {
+  it('Fazendo uma requisição ao HTTP youtube', () => {
     cy.request({
       method: 'POST',
       url: 'https://www.youtube.com/youtubei/v1/log_event?alt=json&key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8'
@@ -75,26 +78,41 @@ describe('Barbearia Alura', () => {
       expect(res.isOkStatusCode).to.be.equal(true)
     })
   })
-  it('Verificando preço da pagina de produtos',()=> {
+  it('Verificando preço da pagina de produtos', () => {
     cy.contains('a', 'Produtos') // visita pagina de produtos
-    .should('be.visible')
-    .click()
-    .title()
-    .should('be.equal', 'Produtos - Barbearia Alura')
-    cy.contains('.produto-preco','R$ 25,00').should('be.visible')
-    cy.contains('.produto-preco','R$ 18,00').should('be.visible')
-    cy.contains('.produto-preco','R$ 35,00').should('be.visible')    
+      .should('be.visible')
+      .click()
+      .title()
+      .should('be.equal', 'Produtos - Barbearia Alura')
+    cy.contains('.produto-preco', 'R$ 25,00').should('be.visible')
+    cy.contains('.produto-preco', 'R$ 18,00').should('be.visible')
+    cy.contains('.produto-preco', 'R$ 35,00').should('be.visible')
   })
-  
-  it('Verificando titulos da pagina de produtos',()=> {
+  it('Verificando titulos da pagina de produtos', () => {
     cy.contains('a', 'Produtos') // visita pagina de produtos
-    .should('be.visible')
-    .click()
-    .title()
-    .should('be.equal', 'Produtos - Barbearia Alura')
-    cy.contains('h2','Cabelo').should('be.visible')
-    cy.contains('h2','Barba').should('be.visible')
-    cy.contains('h2','Cabelo + Barba').should('be.visible')    
+      .should('be.visible')
+      .click()
+      .title()
+      .should('be.equal', 'Produtos - Barbearia Alura')
+    cy.contains('h2', 'Cabelo').should('be.visible')
+    cy.contains('h2', 'Barba').should('be.visible')
+    cy.contains('h2', 'Cabelo + Barba').should('be.visible')
   })
-
+  it('preenche os campos obrigatórios e envia o formulário', () => {
+    const testLong = 'Precisamos aprender Cypress URGENTE para entrarmos na area de analista de teste automatizado Precisamos aprender Cypress URGENTE para entrarmos na area de analista de teste automatizado Precisamos aprender Cypress URGENTE para entrarmos na area de analista de teste automatizado Precisamos aprender Cypress URGENTE para entrarmos na area de analista de teste automatizado'; // variavel com texto
+    cy.contains('a', 'Contato') // visita pagina de contato
+      .should('be.visible')
+      .click()
+      .title()
+      .should('be.equal', 'Contato - Barbearia Alura')
+      cy.get('#nomesobrenome').type('Leonardo Oliveira').should('have.value','Leonardo Oliveira')
+      cy.get('#email').type('leonardoluz@email.com').should('have.value','leonardoluz@email.com')
+      cy.get('#telefone').type('11945587569').should('have.value','11945587569')
+      cy.get('#mensagem').should('be.visible').type(testLong,{delay:0})
+      cy.get('input[type="radio"][value="email"]') // identificando o elemento input
+      .check() // encadeou o check para marcar o radio button ou seja da check no feedback
+      .should('have.value', 'email')
+      cy.contains('.enviar', 'Enviar formulário').click();
+      
+  })
 })
